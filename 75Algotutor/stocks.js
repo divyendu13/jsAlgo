6a89d4bb6b79dbca40eceeae7f69e42aaa0928de
divyendu13 <devd1310@gmail.com>
2023-08-02 21:45:52 +0530

    all files

--- 75Algotutor/stocks.js
+++ 75Algotutor/stocks.js
@@ -1,9 +1,18 @@
-const Prices = [7,1,5,3,6,4]
-let days=Prices.length
-let min=Math.min(...Prices)
-let maxProf =0
-let Prof
-let max=Math.max(...Prices)
+const inp1 = [7,1,5,6,3,4]
 
+function maxProf(Prices){
+    let days=Prices.length
+    let min=Math.min(...Prices)
+    let maxProf =0
+    let Prof
+     
+     for(var i=0;i<days-1;i++){  
+       Prof=Prices[i+1]-min
+       if(Prof>maxProf){
+         maxProf=Prof
+       }
+     }
+     return maxProf
+}
 
-console.log(min)
+console.log(maxProf(inp1))
