5fd5c2ec4ac233395fb5e2586d5e23a481d852fa
divyendu13 <devd1310@gmail.com>
2023-08-03 15:49:01 +0530

    twoSum update logic

--- 75Algotutor/twoSum.js
+++ 75Algotutor/twoSum.js
@@ -1,5 +1,5 @@
-let input = [2,7,11,15] 
-let Target=23 
+let input = [2,7,11,11,15] 
+let Target=22 
 
 function twoSum(inp,target){
     //an empty array to store output
@@ -8,9 +8,9 @@ function twoSum(inp,target){
     //subtracting target from first element and checking remainder exist in array
     for (var i=0;i<inp.length;i++){
         diff = target - inp[i];
-        let j=inp.indexOf(diff)
+        let j=inp.indexOf(diff,i+1)
         if(j>0 && j!=i){
-            index.push(inp.indexOf(diff))
+            index.push(j)
             index.push(i)
             if(index.length==2){break;}
         }
