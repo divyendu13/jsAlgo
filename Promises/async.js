a3ea15cab17c2675c7909d180bf54c208655455f
Divyendu Shukla <devd1310@gmail.com>
2024-04-13 11:31:58 +0530

    Asynchronous JS

--- Promises/async.js
+++ Promises/async.js
@@ -5,3 +5,37 @@
 //When call stack gets empty, JS checks for Message Queue
 //JS engine creates the execution context in the call stack for the call back func and executes it
 //this process is called Event Loop
+
+function print1(){
+    const number=1
+    console.log(number)
+}
+
+function print2(){
+    function number2(){
+        return 2
+    }
+
+    const number=number2()
+    console.log(number)
+}
+
+function print3(){
+    const fs = require('fs')
+    // fs.readFile('D:\\repos\\jsAlgo\\Promises\\readFile.txt','utf-8',function(err,number3){
+    //     console.log(number3)
+    // })
+    const number3 = fs.readFileSync('D:\\repos\\jsAlgo\\Promises\\readFile.txt',{encoding:'utf-8'})
+    console.log(number3)
+}
+
+function print4(){
+    const number4=4
+    console.log(number4)
+}
+
+print1()
+print2()
+print3()
+print4()
+
---
+++ Promises/promise.js
@@ -0,0 +1,23 @@
+/**
+ * Three states - Pending, Fullfilled, Reject.
+ * Fullfilled and Reject are final state
+ */
+
+function sqNumber(num){
+    const promise=new Promise(function(resovle,reject){
+        setTimeout(()=>{
+            if(typeof num != 'number'){
+                return reject(new Error('Argument of type number is expected'))
+            }
+            const result = num*num
+            resovle(result)
+        },1000)
+    })
+    return promise
+}
+
+sqNumber('5').then((value)=>{
+    console.log("Success "+value)
+},reason =>{
+    console.log('Error '+ reason)
+})
